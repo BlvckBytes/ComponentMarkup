bbe3a1bb05a30adad832a736db190bb797df87fb
Philip Hell <blvckbytes@gmail.com>
2025-06-22 17:50:53 +0200

    extracted parser convenience test-APIs into a base-class

--- src/test/java/at/blvckbytes/component_markup/parser/AstParserTests.java
+++ src/test/java/at/blvckbytes/component_markup/parser/AstParserTests.java
@@ -1,31 +1,9 @@
 package at.blvckbytes.component_markup.parser;
 
-import at.blvckbytes.component_markup.ast.ImmediateExpression;
-import at.blvckbytes.component_markup.ast.node.AstNode;
-import at.blvckbytes.component_markup.ast.node.content.TextNode;
-import at.blvckbytes.component_markup.ast.node.content.TranslateNode;
-import at.blvckbytes.component_markup.ast.node.control.ConditionalNode;
-import at.blvckbytes.component_markup.ast.node.control.ContainerNode;
-import at.blvckbytes.component_markup.ast.node.control.IfThenElseNode;
-import at.blvckbytes.component_markup.ast.tag.built_in.BuiltInTagRegistry;
-import at.blvckbytes.component_markup.xml.CursorPosition;
 import at.blvckbytes.component_markup.xml.TextWithAnchors;
-import at.blvckbytes.component_markup.xml.XmlEventParser;
-import at.blvckbytes.component_markup.xml.event.CursorPositionEvent;
-import me.blvckbytes.gpeee.GPEEE;
-import me.blvckbytes.gpeee.IExpressionEvaluator;
-import me.blvckbytes.gpeee.parser.expression.AExpression;
-import org.jetbrains.annotations.Nullable;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.logging.Logger;
-
-public class AstParserTests {
-
-  private static final IExpressionEvaluator expressionEvaluator = new GPEEE(Logger.getAnonymousLogger());
+public class AstParserTests extends AstParserTestsBase {
 
   @Test
   public void shouldParseSimpleCase() {
@@ -170,60 +148,5 @@ public class AstParserTests {
         )
         .child(text(imm("after"), anchor(text, 11)))
     );
-  }
-
-  @SafeVarargs
-  private static NodeWrapper<IfThenElseNode> ifThenElse(@Nullable NodeWrapper<?> wrappedFallback, NodeWrapper<ConditionalNode>... wrappedConditions) {
-    List<ConditionalNode> conditions = new ArrayList<>();
-
-    for (NodeWrapper<ConditionalNode> wrappedCondition : wrappedConditions)
-      conditions.add(wrappedCondition.get());
-
-    return new NodeWrapper<>(new IfThenElseNode(conditions, wrappedFallback == null ? null : wrappedFallback.get()));
-  }
-
-  private static AExpression imm(String value) {
-    return ImmediateExpression.of(value);
-  }
-
-  private static AExpression expr(String expression) {
-    return expressionEvaluator.parseString(expression);
-  }
-
-  private static NodeWrapper<ConditionalNode> conditional(AExpression condition, NodeWrapper<?> wrappedBody) {
-    return new NodeWrapper<>(new ConditionalNode(condition, wrappedBody.get(), new ArrayList<>()));
-  }
-
-  private static NodeWrapper<ContainerNode> container(CursorPosition position) {
-    return new NodeWrapper<>(new ContainerNode(position, new ArrayList<>(), new ArrayList<>()));
-  }
-
-  private static NodeWrapper<TranslateNode> translate(AExpression key, CursorPosition position, @Nullable NodeWrapper<?> wrappedFallback, NodeWrapper<?>... wrappedWiths) {
-    List<AstNode> withs = new ArrayList<>();
-
-    for (NodeWrapper<?> wrappedWith : wrappedWiths)
-      withs.add(wrappedWith.get());
-
-    return new NodeWrapper<>(new TranslateNode(key, withs, wrappedFallback == null ? null : wrappedFallback.get(), position, new ArrayList<>()));
-  }
-
-  private static NodeWrapper<TextNode> text(AExpression value, CursorPosition position) {
-    return new NodeWrapper<>(new TextNode(value, position, new ArrayList<>()));
-  }
-
-  private static CursorPosition anchor(TextWithAnchors text, int anchorIndex) {
-    CursorPositionEvent positionEvent = text.getAnchor(anchorIndex);
-
-    if (positionEvent == null)
-      throw new IllegalStateException("Required anchor at index " + anchorIndex);
-
-    return positionEvent.position;
-  }
-
-  private static void makeCase(TextWithAnchors input, NodeWrapper<?> wrappedExpectedAst) {
-    AstParser parser = new AstParser(BuiltInTagRegistry.get(), expressionEvaluator);
-    XmlEventParser.parse(input.text, parser);
-    AstNode actualAst = parser.getResult();
-    Assertions.assertEquals(wrappedExpectedAst.get().stringify(0), actualAst.stringify(0));
   }
 }
---
+++ src/test/java/at/blvckbytes/component_markup/parser/AstParserTestsBase.java
@@ -0,0 +1,83 @@
+package at.blvckbytes.component_markup.parser;
+
+import at.blvckbytes.component_markup.ast.ImmediateExpression;
+import at.blvckbytes.component_markup.ast.node.AstNode;
+import at.blvckbytes.component_markup.ast.node.content.TextNode;
+import at.blvckbytes.component_markup.ast.node.content.TranslateNode;
+import at.blvckbytes.component_markup.ast.node.control.ConditionalNode;
+import at.blvckbytes.component_markup.ast.node.control.ContainerNode;
+import at.blvckbytes.component_markup.ast.node.control.IfThenElseNode;
+import at.blvckbytes.component_markup.ast.tag.built_in.BuiltInTagRegistry;
+import at.blvckbytes.component_markup.xml.CursorPosition;
+import at.blvckbytes.component_markup.xml.TextWithAnchors;
+import at.blvckbytes.component_markup.xml.XmlEventParser;
+import at.blvckbytes.component_markup.xml.event.CursorPositionEvent;
+import me.blvckbytes.gpeee.GPEEE;
+import me.blvckbytes.gpeee.IExpressionEvaluator;
+import me.blvckbytes.gpeee.parser.expression.AExpression;
+import org.jetbrains.annotations.Nullable;
+import org.junit.jupiter.api.Assertions;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+public abstract class AstParserTestsBase {
+
+  private static final IExpressionEvaluator expressionEvaluator = new GPEEE(Logger.getAnonymousLogger());
+
+  @SafeVarargs
+  protected static NodeWrapper<IfThenElseNode> ifThenElse(@Nullable NodeWrapper<?> wrappedFallback, NodeWrapper<ConditionalNode>... wrappedConditions) {
+    List<ConditionalNode> conditions = new ArrayList<>();
+
+    for (NodeWrapper<ConditionalNode> wrappedCondition : wrappedConditions)
+      conditions.add(wrappedCondition.get());
+
+    return new NodeWrapper<>(new IfThenElseNode(conditions, wrappedFallback == null ? null : wrappedFallback.get()));
+  }
+
+  protected static AExpression imm(String value) {
+    return ImmediateExpression.of(value);
+  }
+
+  protected static AExpression expr(String expression) {
+    return expressionEvaluator.parseString(expression);
+  }
+
+  protected static NodeWrapper<ConditionalNode> conditional(AExpression condition, NodeWrapper<?> wrappedBody) {
+    return new NodeWrapper<>(new ConditionalNode(condition, wrappedBody.get(), new ArrayList<>()));
+  }
+
+  protected static NodeWrapper<ContainerNode> container(CursorPosition position) {
+    return new NodeWrapper<>(new ContainerNode(position, new ArrayList<>(), new ArrayList<>()));
+  }
+
+  protected static NodeWrapper<TranslateNode> translate(AExpression key, CursorPosition position, @Nullable NodeWrapper<?> wrappedFallback, NodeWrapper<?>... wrappedWiths) {
+    List<AstNode> withs = new ArrayList<>();
+
+    for (NodeWrapper<?> wrappedWith : wrappedWiths)
+      withs.add(wrappedWith.get());
+
+    return new NodeWrapper<>(new TranslateNode(key, withs, wrappedFallback == null ? null : wrappedFallback.get(), position, new ArrayList<>()));
+  }
+
+  protected static NodeWrapper<TextNode> text(AExpression value, CursorPosition position) {
+    return new NodeWrapper<>(new TextNode(value, position, new ArrayList<>()));
+  }
+
+  protected static CursorPosition anchor(TextWithAnchors text, int anchorIndex) {
+    CursorPositionEvent positionEvent = text.getAnchor(anchorIndex);
+
+    if (positionEvent == null)
+      throw new IllegalStateException("Required anchor at index " + anchorIndex);
+
+    return positionEvent.position;
+  }
+
+  protected static void makeCase(TextWithAnchors input, NodeWrapper<?> wrappedExpectedAst) {
+    AstParser parser = new AstParser(BuiltInTagRegistry.get(), expressionEvaluator);
+    XmlEventParser.parse(input.text, parser);
+    AstNode actualAst = parser.getResult();
+    Assertions.assertEquals(wrappedExpectedAst.get().stringify(0), actualAst.stringify(0));
+  }
+}
