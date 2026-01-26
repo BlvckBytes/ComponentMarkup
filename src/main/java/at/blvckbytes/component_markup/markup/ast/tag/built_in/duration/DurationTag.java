b5b6e3a3d81a5d7528bc7f16d2842b1f627a256a
BlvckBytes <blvckbytes@gmail.com>
2026-01-26 09:25:52 +0100

    the <duration/>-tag now supports a fractional smallest unit if a custom renderer is provided

--- src/main/java/at/blvckbytes/component_markup/markup/ast/tag/built_in/duration/DurationTag.java
+++ src/main/java/at/blvckbytes/component_markup/markup/ast/tag/built_in/duration/DurationTag.java
@@ -46,19 +46,37 @@ public class DurationTag extends TagDefinition {
     return new FunctionDrivenNode(tagName, interpreter -> {
       TemporaryMemberEnvironment environment = interpreter.getEnvironment();
 
-      long duration = interpreter.evaluateAsLong(value);
+      double duration = interpreter.evaluateAsDouble(value);
       List<DurationUnit> requestedUnits = parseUnitsString(interpreter, units);
       boolean keepZeroes = zeroes != null && interpreter.evaluateAsBoolean(zeroes);
 
       requestedUnits.sort((a, b) -> Long.compare(b.milliseconds, a.milliseconds));
 
-      long[] unitValues = new long[requestedUnits.size()];
-      long remaining = duration;
+      double[] unitValues = new double[requestedUnits.size()];
+      double remaining = duration;
 
       for (int requestedUnitIndex = 0; requestedUnitIndex < requestedUnits.size(); ++requestedUnitIndex) {
         DurationUnit requestedUnit = requestedUnits.get(requestedUnitIndex);
-        long unitValue = remaining / requestedUnit.milliseconds;
-        remaining %= requestedUnit.milliseconds;
+        double unitValue = remaining / requestedUnit.milliseconds;
+        boolean isLastUnit = requestedUnitIndex == requestedUnits.size() - 1;
+
+        if (!isLastUnit)
+          unitValue = Math.floor(unitValue);
+
+        else {
+          // Account for precision-oddities; better safe than sorry!
+          if (unitValue < .001)
+            unitValue = 0;
+
+          // Without an explicit unit-renderer, we always discard the fractional part, seeing how
+          // it requires proper formatting, which we are not going to take care of (separation
+          // of concerns - simply employ the number-tag in the unit-renderer).
+          if (unitRenderer == null)
+            unitValue = Math.floor(unitValue);
+        }
+
+        while (remaining > requestedUnit.milliseconds)
+          remaining -= requestedUnit.milliseconds;
 
         unitValues[requestedUnitIndex] = unitValue;
       }
@@ -67,7 +85,7 @@ public class DurationTag extends TagDefinition {
 
       for (int requestedUnitIndex = 0; requestedUnitIndex < requestedUnits.size(); ++requestedUnitIndex) {
         DurationUnit requestedUnit = requestedUnits.get(requestedUnitIndex);
-        long unitValue = unitValues[requestedUnitIndex];
+        double unitValue = unitValues[requestedUnitIndex];
 
         if (unitValue == 0 && !keepZeroes)
           continue;
@@ -78,13 +96,24 @@ public class DurationTag extends TagDefinition {
         didEmit = true;
 
         if (unitRenderer == null) {
-          interpreter.interpret(new TextNode(InputView.EMPTY, String.valueOf(unitValue) + requestedUnit.character), null);
+          interpreter.interpret(new TextNode(InputView.EMPTY, String.valueOf((int) unitValue) + requestedUnit.character), null);
           continue;
         }
 
         interpreter.interpret(unitRenderer, () -> {
-          environment.setScopeVariable("value", unitValue);
           environment.setScopeVariable("unit", String.valueOf(requestedUnit.character));
+
+          boolean isFractional = unitValue != Math.floor(unitValue);
+
+          environment.setScopeVariable("is_fractional", isFractional);
+
+          // Prefer integers if there's no fractional part, as they stringify more desirable by default.
+          if (!isFractional) {
+            environment.setScopeVariable("value", (int) unitValue);
+            return;
+          }
+
+          environment.setScopeVariable("value", unitValue);
         });
       }
 
--- src/test/java/at/blvckbytes/component_markup/markup/interpreter/tag/DurationTagTests.java
+++ src/test/java/at/blvckbytes/component_markup/markup/interpreter/tag/DurationTagTests.java
@@ -17,7 +17,7 @@ public class DurationTagTests extends InterpreterTestsBase {
   // TODO: This could use a few more detailed cases
 
   @Test
-  public void shouldRenderWithDefaultFormat() {
+  public void shouldRenderWithDefaultRenderer() {
     TextWithSubViews text = new TextWithSubViews(
       "<duration value=98000 units=\"s\" />"
     );
@@ -40,6 +40,47 @@ public class DurationTagTests extends InterpreterTestsBase {
       SlotType.CHAT,
       new JsonObjectBuilder()
         .string("text", "1m 38s")
+    );
+  }
+
+  @Test
+  public void shouldNotRenderFractionalWithDefaultRenderer() {
+    TextWithSubViews text = new TextWithSubViews(
+      "<duration",
+      "  [value]='(60 + 30.5) * 1000'",
+      "  units='ms'",
+      "/>"
+    );
+
+    makeCase(
+      text,
+      new InterpretationEnvironment(),
+      SlotType.CHAT,
+      new JsonObjectBuilder()
+        .string("text", "1m 30s")
+    );
+  }
+
+  @Test
+  public void shouldRenderFractionalWithCustomRenderer() {
+    TextWithSubViews text = new TextWithSubViews(
+      "<duration",
+      "  [value]='(60 + 30.5) * 1000'",
+      "  units='ms'",
+      "  unit-renderer={",
+      "    <number *if='is_fractional' [value]='value' format='0.00' locale='de_DE' />",
+      "    <container *else &value />",
+      "    {unit}",
+      "  }",
+      "/>"
+    );
+
+    makeCase(
+      text,
+      new InterpretationEnvironment(),
+      SlotType.CHAT,
+      new JsonObjectBuilder()
+        .string("text", "1m 30,50s")
     );
   }
 }
